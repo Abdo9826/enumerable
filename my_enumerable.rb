d010a1b2f8f3ed4f91b95e1361ac30f90ef793b2
Olawale <olapetanwale@outlook.com>
2022-08-19 15:45:48 +0100

    Add List class that inherits from my_enumerable.rb

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
+# Created my_enumerables and functions to use in the list.rb file
 module MyEnumerable
-    def all?(&block)
-      each { |value| return false unless block.call(value) }
-      true
-    end
-  
-    def any?(&block)
-      each { |value| return true if block.call(value) }
-      false
-    end
-  
-    def filter(&block)
-      result = []
-      each { |value| result << value if block.call(value) }
-      result
-    end
+  def all?(&block)
+    each { |value| return false unless block.call(value) }
+    true
   end
-  
+
+  def any?(&block)
+    each { |value| return true if block.call(value) }
+    false
+  end
+
+  def filter(&block)
+    result = []
+    each { |value| result << value if block.call(value) }
+    result
+  end
+end
